c6136b4826fae0f290735ca1052efdd72a4a8595
Your Name~manojprabhakar24 <manojptn16@gmail.com>
2023-09-25 11:56:30 +0530

    ok

--- lib/Screens/SecondScreen.dart
+++ lib/Screens/SecondScreen.dart
@@ -10,7 +10,18 @@ class SecondScreen extends StatelessWidget {
       body: Center(
           child: Column(
             children: [
-              const Text("Second Screen"),
+              Container(
+                margin: EdgeInsets.all(20),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Manoj Prabhakar", style: TextStyle(fontSize: 73),),
+
+              ),
+              Image.asset('images/subhman.png',
+                width: 350,
+                height: 250,),
               ElevatedButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/third');
--- lib/Screens/ThirdScreen.dart
+++ lib/Screens/ThirdScreen.dart
@@ -10,12 +10,20 @@ class ThirdScreen extends StatelessWidget {
       body: Center(
           child: Column(
             children: [
-              const Text("Third Screen"),
+              Container(
+                margin: EdgeInsets.all(20),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Thank You", style: TextStyle(fontSize: 55),),
+
+              ),
               ElevatedButton(
                   onPressed: () {
                     Navigator.of(context).popUntil((route) => route.isFirst);
                   },
-                  child: const Text("Go To Next"))
+                  child: const Text("Home"))
             ],
           )),
     );
